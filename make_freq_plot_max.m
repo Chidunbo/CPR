a5e42733ea8c08cc74737b400af0572b9d1b0a17
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-12-01 14:28:42 -0500

    Change in class

--- make_freq_plot_max.m
+++ make_freq_plot_max.m
@@ -16,13 +16,13 @@ freq_domain = linspace(-Fs/2, Fs/2-Fs/length(x),length(x));
 plot(freq_domain, 1/length(x)*fftshift(abs(fft(x))))
 
 % find top 2 maximum amplitude value and corresponding index
-[max_amp, max_i] = maxk(1/length(x)*fftshift(abs(fft(x))),5);
+[max_amp, max_i] = maxk(1/length(x)*fftshift(abs(fft(x))),3);
 
 % find maximum frequency
 max_freq = freq_domain(max_i);
 
 % take only the positive max frequency
-max_freq = max(max_freq(max_freq > 0)); 
+max_freq = max_freq(max_freq > 0); 
 
 title("data in frequency domain")
 ylabel('Magnitude of signal');
