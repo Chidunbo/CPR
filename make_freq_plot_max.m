43eeec205d576b473958fb742b069adf6211d9ca
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-12-12 13:45:57 -0500

    Add force score and change zero freq to .0001

--- make_freq_plot_max.m
+++ make_freq_plot_max.m
@@ -1,29 +1,43 @@
-function [max_amp,max_freq]  = make_freq_plot_max(x, Fs)
+function [max_amp,max_freq,zero_amp]  = make_freq_plot_max(x, Fs)
 
-% make a frequency domain plot with frequency labels
-% and output the max amplitude and the corresponding dominant frequency(Hz)
-%
+% Calcuate max amplitude (which represent acceleration in m/s2)
+% and the corresponding dominant non-zero frequency(Hz), and find the
+% amplitude of 0Hz frequency.
+%   Args:
 %   x = input signal
-%   Fs = sampling frequency in Hz
+%   Fs = sampling frequency (Hz)
+%   Outputs: 
+%   max_amp = acceleration of dominate non-zero frequency (m/s2)
+%   max_freq = dominate frequency (Hz)
 
+% make number of data samples even
 if(mod(length(x),2) ~=0)
     x = x(1:end-1);
 end
+
 % frequency domain
 freq_domain = linspace(-Fs/2, Fs/2-Fs/length(x),length(x));
 
 % plot signal over frequency domain
 plot(freq_domain, 1/length(x)*fftshift(abs(fft(x))))
-
-% find top 2 maximum amplitude value and corresponding index
-[max_amp, max_i] = maxk(1/length(x)*fftshift(abs(fft(x))),3);
-
-% find maximum frequency
-max_freq = freq_domain(max_i);
-
-% take only the positive max frequency
-max_freq = max_freq(max_freq > 0); 
-
 title("data in frequency domain")
 ylabel('Magnitude of signal');
 xlabel('Frequency (Hz)');
+
+% find top 3 maximum amplitude values and corresponding indeces
+[amps, is] = maxk(1/length(x)*fftshift(abs(fft(x))),3);
+
+% find maximum frequency
+freqs = freq_domain(is);
+
+% find acceleration offset
+i_zero = find(abs(freqs) < 0.000001);
+zero_amp = amps(i_zero);
+
+% take only the positive max frequency
+max_freq = freqs(freqs > 0); 
+
+% find corresponding amp
+max_i = find(freqs == max_freq);
+max_amp = amps(max_i);
+
